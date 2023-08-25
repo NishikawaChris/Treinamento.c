3d90a4bf00f20e18cd8ef2002f30e8da818f3161
NishikawaChris <142899736+NishikawaChris@users.noreply.github.com>
2023-08-25 13:40:11 +0000

    bovino com for

--- exercicio2.c
+++ exercicio2.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
 int main(){
-    int numbovinos, ano, bovinicial, nascimento, morte;
+    int numbovinos, ano = 0, atual, nascimento, morte, final;
 
     printf("Informe a quantidade de bovinos: ");
-    scanf("%d", numbovinos);
+    scanf("%d", &numbovinos);
 
-    for(int bovinicial == 10; )
+    for(int atual = 10; atual < numbovinos; atual = atual + (atual / 3) - (atual /4)){
+        ano++;
+    }
+    printf("ano = %d\n", ano);
 }
